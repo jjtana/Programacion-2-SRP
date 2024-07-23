dbe09f92ad00441241c37b0d6edfc7d8b8988412
jjtana <dexydax@gmail.com>
2024-07-22 23:41:26 -0400

    Add project files.

---
+++ ConsoleApp25/Persona.cs
@@ -0,0 +1,39 @@
+﻿public class Persona
+{
+    private string nombre;
+    private int edad;
+    private string direccion;
+    private string correoElectronico;
+
+    public Persona(string nombre, int edad, string direccion, string correoElectronico)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public string Nombre => nombre;
+    public int Edad => edad;
+    public string Direccion => direccion;
+    public string CorreoElectronico => correoElectronico;
+}
+
+public class CorreoService
+{
+    public void EnviarCorreoElectronico(string mensaje, Persona persona)
+    {
+        // Lógica para enviar correo electrónico
+    }
+}
+
+public class ImprimirService
+{
+    public void ImprimirDatos(Persona persona)
+    {
+        Console.WriteLine($"Nombre: {persona.Nombre}");
+        Console.WriteLine($"Edad: {persona.Edad}");
+        Console.WriteLine($"Dirección: {persona.Direccion}");
+        Console.WriteLine($"Correo electrónico: {persona.CorreoElectronico}");
+    }
+}
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            Persona persona = new Persona("Jostin Wilmer", 20, "Calle Manuel Maria De Corso", "jostinwilmer@hotmail.com");
+
+            
+            CorreoService correoService = new CorreoService();
+            ImprimirService imprimirService = new ImprimirService();
+
+           
+            imprimirService.ImprimirDatos(persona);
+            correoService.EnviarCorreoElectronico("Hola, este es un mensaje de prueba.", persona);
+        }
+    }
+}
